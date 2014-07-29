3b2ce5f8a5671960af050f71224394574b02f983
kimx <kimxinfo@gmail.com>
2014-07-29 22:06:34 +0800

    1

---
+++ AngularLab/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AngularLab
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+              name: "Project_Info",
+              url: "{controller}/{action}/{projectId}"
+              , defaults: new { projectId = UrlParameter.Optional }
+              , constraints: new { controller="Info"}
+          );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
