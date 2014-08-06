95731a7e1254a6ddc50a252a4fcda4e78a2b0f6d
kimx <kimxinfo@gmail.com>
2014-08-06 09:47:08 +0800

    1

---
+++ AngularLab/Controllers/TutorialController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularLab.Controllers
+{
+    public class TutorialController : Controller
+    {
+        // GET: Tutorial
+        public ActionResult Templates()
+        {
+            return View();
+        }
+        public ActionResult Filtering()
+        {
+            return View();
+        }
+        public ActionResult DataBinding()
+        {
+            return View();
+        }
+        public ActionResult DependencyInjection()
+        {
+            return View();
+        }
+
+        public ActionResult TemplatingLinksImages()
+        {
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            return View();
+        }
+
+        public ActionResult RoutingMultipleIndex()
+        {
+            return View();
+        }
+        public ActionResult RoutingMultipleTabMain()
+        {
+            return PartialView();
+        }
+
+        public ActionResult RoutingMultipleTabDetail()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ AngularLab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularLab.WebApiApplication" Language="C#" %>
