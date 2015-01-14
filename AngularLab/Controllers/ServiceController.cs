871fc20d4c8d02f2d554bf6bca88babb6d4e58d4
kimx <kimxinfo@gmail.com>
2015-01-14 15:47:26 +0800

    Provider

---
+++ AngularLab/Controllers/ServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularLab.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: Service
+        public ActionResult Provider()
+        {
+            return View();
+        }
+    }
+}
