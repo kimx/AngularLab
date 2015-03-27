715582fd0fa39ea658a18d71673b276ccf158003
kimx <gn01124620@gmail.com>
2015-03-27 13:49:09 +0800

    1

---
+++ AngularLab/Controllers/DirectiveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularLab.Controllers
+{
+    public class DirectiveController : Controller
+    {
+        // GET: Directive
+        public ActionResult PassParas()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularLab/Controllers/XeditableController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularLab.Controllers
+{
+    public class XeditableController : Controller
+    {
+        // GET: Xeditable
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DIYIndex()
+        {
+            return View();
+        }
+
+        public ActionResult DIYIndex2()
+        {
+            return View();
+        }
+
+        public ActionResult DIYNgTable()
+        {
+            return View();
+        }
+    }
+}
