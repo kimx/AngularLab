c059b6107c8f96719f4e85413f7ba4f2604f89f5
kimx <gn01124620@gmail.com>
2015-04-17 22:27:04 +0800

    1

---
+++ AngularLab/Controllers/GridLabController.cs
@@ -0,0 +1,24 @@
+﻿using AngularLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularLab.Controllers
+{
+    public class GridLabController : Controller
+    {
+        // GET: GridLab
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult QueryProducts()
+        {
+            var data = ProductEntity.GetDatas();
+            return Json(new { data }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
